b51eb45c2c9798454ba36a4ebf8d8b901ed4fa04
mrfaizanshariff <mrfaizanshariff@gmail.com>
2021-09-03 16:40:20 +0530

    The code has been optimized using the ternary operator

--- script.js
+++ script.js
@@ -3,10 +3,15 @@
 let secretNumber = Math.trunc(Math.random() * 20 + 1);
 // document.querySelector('.number').textContent = secretNumber;
 let highScore = 0;
-// let count = 0;
-
+//function to set the message box
+function messageBlock(message) {
+  document.querySelector('.message').textContent = message;
+}
 let score = 20;
-
+//function to set the score
+function setScore(score) {
+  document.querySelector('.score').textContent = score;
+}
 //Check Button On Click
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -27,34 +32,21 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.highscore').textContent = highScore;
     }
   }
-  //when guess is high
-  else if (guess > secretNumber) {
+  //when guess is wrong
+  else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = 'Too High';
+      guess > secretNumber ? messageBlock(`Too High`) : messageBlock(`Too Low`);
       score--;
-      document.querySelector('.score').textContent = score;
+      // document.querySelector('.score').textContent = score;
+      setScore(score);
 
       document.querySelector('body').style.backgroundColor = '#20B7B4';
     } else {
       score--;
-      document.querySelector('.score').textContent = score;
-
-      document.querySelector('.message').textContent = 'You Lost The Game 🤦‍♂️';
-      document.querySelector('body').style.backgroundColor = '#F33838';
-      document.querySelector('.number').textContent = secretNumber;
-    }
-  }
-  //when guess is low
-  else if (guess < secretNumber) {
-    if (score > 1) {
-      score--;
-      document.querySelector('.message').textContent = 'Too Low';
-      document.querySelector('.score').textContent = score;
-      document.querySelector('body').style.backgroundColor = '#6920B6';
-    } else {
-      score--;
-      document.querySelector('.score').textContent = score;
-      document.querySelector('.message').textContent = 'You Lost The Game 🤦‍♂️';
+      // document.querySelector('.score').textContent = score;
+      setScore(score);
+      messageBlock(`you lost the game 🤦‍♂️`);
+      // document.querySelector('.message').textContent = 'You Lost The Game 🤦‍♂️';
       document.querySelector('body').style.backgroundColor = '#F33838';
       document.querySelector('.number').textContent = secretNumber;
     }
@@ -64,7 +56,8 @@ document.querySelector('.check').addEventListener('click', function () {
 document.querySelector('.again').addEventListener('click', function () {
   //score reset
   score = 20;
-  document.querySelector('.score').textContent = score;
+  // document.querySelector('.score').textContent = score;
+  setScore(score);
   //random number reset
   secretNumber = Math.trunc(Math.random() * 20 + 1);
 
@@ -75,9 +68,44 @@ document.querySelector('.again').addEventListener('click', function () {
   //Number Box visibility reset
   document.querySelector('.number').textContent = '?';
   //message box reset
-  document.querySelector('.message').textContent = 'Start guessing...';
+  // document.querySelector('.message').textContent = 'Start guessing...';
+  messageBlock('Start guessing...');
   //input box reset
   document.querySelector('.guess').value = '';
   //incrementing count value
   // count++;
 });
+
+//previous game logic included within the check button onclick
+//when guess is high
+// else if (guess > secretNumber) {
+//   if (score > 1) {
+//     document.querySelector('.message').textContent = 'Too High';
+//     score--;
+//     document.querySelector('.score').textContent = score;
+
+//     document.querySelector('body').style.backgroundColor = '#20B7B4';
+//   } else {
+//     score--;
+//     document.querySelector('.score').textContent = score;
+
+//     document.querySelector('.message').textContent = 'You Lost The Game 🤦‍♂️';
+//     document.querySelector('body').style.backgroundColor = '#F33838';
+//     document.querySelector('.number').textContent = secretNumber;
+//   }
+// }
+// //when guess is low
+// else if (guess < secretNumber) {
+//   if (score > 1) {
+//     score--;
+//     document.querySelector('.message').textContent = 'Too Low';
+//     document.querySelector('.score').textContent = score;
+//     document.querySelector('body').style.backgroundColor = '#6920B6';
+//   } else {
+//     score--;
+//     document.querySelector('.score').textContent = score;
+//     document.querySelector('.message').textContent = 'You Lost The Game 🤦‍♂️';
+//     document.querySelector('body').style.backgroundColor = '#F33838';
+//     document.querySelector('.number').textContent = secretNumber;
+//   }
+// }
